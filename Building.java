df5b441bcfe05b5ffc1cce5dd809ccd785191464
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2015-12-07 22:20:26 -0600

    :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    i didnt actually get a 100
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:
    :100:  :100:

--- Building.java
+++ Building.java
@@ -13,12 +13,13 @@ class Building
    {
       x = (int) ((Math.random() * (max - min)) + min);
       y = (int) ((Math.random() * (max - min)) + min);
-      Graphics g = new Graphics();
+      width = 300;
+      height = 800;
       g.setColor(new Color(0,0,0));
-      g.fillRect(x, y, );   
+      g.fillRect(x, y, width, height);   
    }
    
-   public int get_x() {return x;)
+   public int get_x() {return x;}
    public int get_y() {return y;}
    public int get_width() {return width;}
    public int get_height() {return height;}
@@ -32,21 +33,21 @@ class Building
 
 class CustomBuild extends Building
 {
-   public CustomBuild()
+   public CustomBuild(Graphics g, int max, int min)
    {
-            
+      super(g, max, min);
    }
    
    public void addPatio(Graphics g, int elevation)
    {  
       g.setColor(new Color(0,0,0));
-      g.fillRect((x + width), (y - elevation), 50, 100);
+      g.fillRect((get_x() + get_width()), (get_y() - elevation), 50, 100);
    }
    
    public void addTop(Graphics g, int exceed, int self_height)
    {
       g.setColor(new Color(0,0,0));
-      g.fillRect((x + exceed), y, (width - 30), self_height);
-      g.fillRect((x + exceed + 10), y, 3, 10);
+      g.fillRect((get_x() + exceed), get_y(), (get_width() - 30), self_height);
+      g.fillRect((get_x() + exceed + 10), get_y(), 3, 10);
    }
 }
