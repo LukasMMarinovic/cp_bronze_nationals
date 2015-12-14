8d4f1df1f2b07f4b9266bf7931c7e9eeb69c8446
handroishonda <handro2010@live.com>
2015-12-13 22:23:55 -0600

    peeps on the subs
    added methods for two people on the subway

---
+++ PeopleOnDaSubway.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.applet.*;
+
+class PeopleOnDaSubway
+{
+   public static void sittingBull(Graphics g)    //this is the sitting person
+   {
+      Polygon sittingBull = new Polygon();
+   }
+   
+   public static void standingGuy(Graphics g)    //this is the standing guy
+   {
+      Polygon standingGuy = new Polygon();
+   }
+}
